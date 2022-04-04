8812050a439e2429cd2a65ed8400c1ec3e866a19
lizf-personal-WGwd95oSHWuI <102977933+lizf-personal-WGwd95oSHWuI@users.noreply.github.com>
2022-04-04 07:48:31 -0700

    Create client_config.js

---
+++ MissionControl/client_config.js
@@ -0,0 +1,4 @@
+var WAKER_URL = 'https://script.google.com/macros/s/AKfycbzCZcf0qSb4o6E4Ubrzf3JWz8x_wEs3Lo6FdevBW0vwJFohCDxe44h1KjcgCP9jNaP5Bw/exec';
+var WAKE_TIMEOUT = 8*1000;
+var MESSAGE_BUS_URL = 'https://whispering-savannah-73363.herokuapp.com/';
+var SERVER_NAME = 'MissionControl-Server';
