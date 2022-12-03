38013de513c76ad834c4b8497d6fb0a90e15bf1c
lizf-personal-WGwd95oSHWuI <102977933+lizf-personal-WGwd95oSHWuI@users.noreply.github.com>
2022-12-03 08:41:04 -0800

    Update client_config.js

--- MissionControl/client_config.js
+++ MissionControl/client_config.js
@@ -1,4 +1,4 @@
 var MC_WAKER_URL = 'https://script.google.com/macros/s/AKfycbzCZcf0qSb4o6E4Ubrzf3JWz8x_wEs3Lo6FdevBW0vwJFohCDxe44h1KjcgCP9jNaP5Bw/exec';
 var MC_WAKE_TIMEOUT = 8*1000;
-var MC_MESSAGE_BUS_URL = 'https://whispering-savannah-73363.herokuapp.com/';
+var MC_MESSAGE_BUS_URL = 'https://mc-bus.onrender.com/';
 var MC_SERVER_NAME = 'MissionControl-Server';
